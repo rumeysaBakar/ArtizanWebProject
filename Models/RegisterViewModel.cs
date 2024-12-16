b7a6fbbd56d8a8f442674c4e1b64b76a8fe1ee29
Rumeysa bakar <rumeysa.bakar.81@hotmail.com>
2024-12-16 11:20:37 +0300

    Add files via upload

---
+++ Models/RegisterViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ArtizanWebProject.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+
+        [Phone]
+        [StringLength(15)]
+        public string? Phone { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+
+        
+
+
+    }
+}
+
